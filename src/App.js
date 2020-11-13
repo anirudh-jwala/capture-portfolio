eb32249269ae56ce974597baf79791a9641b3fe8
Anirudh Jwala <anirudhsai2@gmail.com>
2020-11-13 17:59:11 +0530

    Global styling and about us section with Styled components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import React from "react";
 // Pages
 import AboutUs from "./pages/AboutUs";
 
+// Global Style
+import GlobalStyle from "./components/GlobalStyle";
+
 const App = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
