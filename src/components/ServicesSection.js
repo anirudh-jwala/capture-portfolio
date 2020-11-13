6ff435a3824cf2d78220b89d8d4975268b9777f4
Anirudh Jwala <anirudhsai2@gmail.com>
2020-11-13 19:19:20 +0530

    Styling services section

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+// Styled components
+import styled from "styled-components";
+import { About, Description, Image } from "../styles";
+
 // Icons
 import clock from "../img/clock.svg";
 import diaphragm from "../img/diaphragm.svg";
@@ -11,47 +15,77 @@ import home2 from "../img/home2.png";
 
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="desciption">
+    <Services>
+      <Description>
         <h2>
           High <span>quality</span> services
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={clock} alt="Clock" />
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum, dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={teamwork} alt="Teamwork" />
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum, dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={diaphragm} alt="Diaphram" />
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum, dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={money} alt="Money" />
               <h3>Money</h3>
             </div>
             <p>Lorem ipsum, dolor sit amet.</p>
-          </div>
-        </div>
-      </div>
-      <div className="image">
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt="Camera" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServicesSection;
