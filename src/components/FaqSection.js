e9ad8d26ffc5a1b8f7320607fbea74a9831dcf21
Anirudh Jwala <anirudhsai2@gmail.com>
2020-11-13 19:26:11 +0530

    Styling FAQ section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,37 +1,88 @@
 import React from "react";
 
+// Styled components
+import styled from "styled-components";
+import { About } from "../styles";
+
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any questions? <span>FAQ</span>
       </h2>
       <div className="question">
         <h4>How Do I Start?</h4>
         <div className="answer">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, rem.
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+            quae?
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule</h4>
         <div className="answer">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, rem.
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+            quae?
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Different Payment Methods</h4>
         <div className="answer">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, rem.
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+            quae?
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What Products Do You Offer?</h4>
         <div className="answer">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, rem.
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo,
+            quae?
+          </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
