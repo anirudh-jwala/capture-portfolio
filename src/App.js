6af1e2b3835643ebe8aea0adccc874157f83a5e6
Anirudh Jwala <anirudhsai2@gmail.com>
2020-11-13 19:46:26 +0530

    Navigation along with routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 
 // Pages
 import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
 
 // Global Style
 import GlobalStyle from "./components/GlobalStyle";
@@ -9,12 +11,19 @@ import GlobalStyle from "./components/GlobalStyle";
 // Components
 import Nav from "./components/Nav";
 
+// Router
+import { Switch, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" component={AboutUs} exact />
+        <Route path="/work" component={OurWork} exact />
+        <Route path="/contact" component={ContactUs} exact />
+      </Switch>
     </div>
   );
 };
